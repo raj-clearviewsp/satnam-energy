afa28fbd27dbd1431bbcefd44b70c47b128c5239
rajsawhney-model <raj.sawhney.model@gmail.com>
2025-05-15 15:11:32 -0700

    Vercel config: remove next export and old tracker page

--- next.config.js
+++ next.config.js
@@ -1,31 +1,28 @@
+/* next.config.js ------------------------------------------------------- */
+/* MDX support ---------------------------------------------------------- */
 const withMDX = require('@next/mdx')({
-    extension: /\.mdx?$/,
-    options: {
-      // If you use remark-gfm, you'll need to use next.config.mjs
-      // as the package is ESM only
-      // https://github.com/remarkjs/remark-gfm#install
-      remarkPlugins: [],
-      rehypePlugins: [],
-      // If you use `MDXProvider`, uncomment the following line.
-      providerImportSource: "@mdx-js/react",
-    },
-  })
-  
-  /** @type {import('next').NextConfig} */
-  module.exports = {
-    output: 'export',          // tells Next to emit static HTML
-    images: { unoptimized: true }, // disable the Next.js image optimizer
-  };
-  
-  const nextConfig = {
-    // Configure pageExtensions to include md and mdx
-    pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
-    // Optionally, add any other Next.js config below
-    reactStrictMode: true,
-    images: {
-      domains: ['images.unsplash.com'],
-    },
-  }
-  
-  // Merge MDX config with Next.js config
-  module.exports = withMDX(nextConfig)
+  extension: /\.mdx?$/,
+  options: {
+    remarkPlugins: [],
+    rehypePlugins: [],
+    providerImportSource: '@mdx-js/react',
+  },
+});
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  /* -- General -------------------------------------------------------- */
+  reactStrictMode: true,
+
+  /* -- MD / MDX file extensions so you can do `page.mdx` ------------- */
+  pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
+
+  /* -- Remote images you allow `next/image` to optimise -------------- */
+  images: {
+    domains: ['images.unsplash.com'],
+  },
+
+  /* -- (Nothing else needed for Vercel) ------------------------------- */
+};
+
+module.exports = withMDX(nextConfig);
